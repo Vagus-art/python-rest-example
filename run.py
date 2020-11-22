dd275fa47f94a72f32149f72ff86268464707e66
Vagus-art <agustin.favo@gmail.com>
2020-11-22 05:06:21 -0300

    added some routes with basic functions

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, request, url_for
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/methods', methods=["POST","GET"])
+def methodsHandler():
+    if request.method == "POST":
+        return "posted"
+    elif request.method == "GET":
+        return "getted"
+    else:
+        return "not posted nor getted"
+
+@app.route('/echo/<var>')
+def echoesHandler(var=''):
+    return f"You wrote {var}!"
